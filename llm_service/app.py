87ebca99ea6c23c98c9bbbdcb7b5fce5e2ef779f
Hoang Do Khai <khai.hoangdo@ncc.asia>
2025-09-11 15:50:13 +0700

    khai.hoangdo/Change model and fix prompt

--- llm_service/app.py
+++ llm_service/app.py
@@ -1,9 +1,17 @@
+# api/main.py
+import os
 import json
 import requests
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
-from prompt import BASE_PROMPT
+from prompt import BASE_PROMPT_CHAIN_OF_THOUGHT
+from dotenv import load_dotenv
+load_dotenv()
+
+from google import genai
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 app = FastAPI()
 
@@ -16,15 +24,45 @@ async def llm_review(req: DiffRequest):
     if not diff:
         return {"error": "Missing diff"}
 
-    try:
-        prompt = f"""{BASE_PROMPT}
+    prompt = f"""{BASE_PROMPT_CHAIN_OF_THOUGHT}
+                {diff}
+"""
 
+    try:
+        model_id = os.getenv("GEMINI_MODEL")
+
+        response = client.models.generate_content(
+            model=model_id,
+            contents=prompt
+        )
+
+        review_text = response.text if hasattr(response, "text") else str(response)
+
+        if not review_text or review_text.strip() == "":
+            review_text = "No review returned (empty)."
+
+        return {"review": review_text.strip()}
+
+    except Exception as e:
+        return {"error": str(e)}
+
+
+
+@app.post("/llm-review-ollama")
+async def llm_review(req: DiffRequest):
+    diff = req.diff
+    if not diff:
+        return {"error": "Missing diff"}
+
+    try:
+        prompt = f"""{BASE_PROMPT_CHAIN_OF_THOUGHT}
+Diff: 
 {diff}
 """
 
         with requests.post(
             "http://localhost:11434/api/generate",
-            json={"model": "phi3:3.8b", "prompt": prompt},
+            json={"model": "qwen2.5:7b", "prompt": prompt},
             stream=True,
             timeout=300
         ) as resp:
