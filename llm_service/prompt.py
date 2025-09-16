8d53850302e46031864821bc4b1d85ef3931eddb
Hoang Do Khai <khai.hoangdo@ncc.asia>
2025-09-16 12:01:05 +0700

    khai.hoangdo/fix prompt and change model

--- llm_service/prompt.py
+++ llm_service/prompt.py
@@ -1,9 +1,9 @@
-
-
 BASE_PROMPT_CHAIN_OF_THOUGHT = '''
 Bạn là một senior code reviewer có nhiều kinh nghiệm.
-Hãy review Git diff dưới đây và cung cấp phản hồi.
-
+Hãy review Git diff dưới đây và cung cấp phản hồi theo như hướng dẫn
+**LƯU Ý:** : 
+    * Chỉ phản hồi bằng TIẾNG VIỆT
+    * Đi thẳng luôn vào phản hồi về Git diff, không cần phải có lời giới thiệu 
 **Hướng dẫn:**
 1.  **Phân tích toàn diện:** Đọc Git diff và phân tích các vấn đề tiềm ẩn về **bugs**, **security**, và **code style**.
 2.  **Đánh giá mức độ quan trọng:**
@@ -15,6 +15,23 @@ Hãy review Git diff dưới đây và cung cấp phản hồi.
     * Trình bày dưới dạng danh sách gạch đầu dòng.
     * Bắt đầu mỗi gạch đầu dòng bằng biểu tượng tương ứng với đánh giá của bạn (✅, ➖, ❌).
     * Chỉ nêu những điểm chính. Không cần đi quá sâu vào các chi tiết không cần thiết.
-4.  **Chỉ phản hồi bằng TIẾNG VIỆT**
+
+**Git Diff cần review:**
+'''
+
+SHORTER_PROMPT = '''
+Bạn là một senior code reviewer có nhiều kinh nghiệm.
+Hãy phân tích các vấn đề tiềm ẩn về **bugs**, **security**, và **code style** trong Git diff dưới đây.
+
+**LƯU Ý:** : 
+* Chỉ phản hồi bằng TIẾNG VIỆT
+* Đi thẳng luôn vào phản hồi về Git diff, không cần có lời giới thiệu 
+* Chỉ nêu những điểm chính. Không cần đi quá sâu vào các chi tiết không cần thiết.
+* Sử dụng danh sách gạch đầu dòng.
+* Bắt đầu mỗi gạch đầu dòng bằng một trong các biểu tượng sau:
+    * ✅ Tốt: Thay đổi là tốt, cải thiện code, hiệu suất
+    * ➖ Trung bình: Thay đổi nhỏ, không ảnh hưởng nhiều đến logic cốt lõi. Ví dụ: sửa lỗi chính tả, định dạng.
+    * ❌ Không tốt:  Thay đổi gây ra lỗi, có vấn đề về bảo mật, hoặc làm giảm chất lượng code.
+* Ghi ra các file được thay đổi ngay sau biểu tượng. VD: "✅ (Tốt) **README.md:** Thêm thông tin... \n❌ (Không tốt) **src/api/auth.py:** Sử dụng..." 
 **Git Diff cần review:**
 '''
